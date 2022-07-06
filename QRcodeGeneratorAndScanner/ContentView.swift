50dd0b45e39571efdb560236d88bca87088f5eb4
Denis Goloborodko <goldena_by@outlook.com>
2022-07-06 23:59:57 +0300

    Initial Commit

---
+++ QRcodeGeneratorAndScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRcodeGeneratorAndScanner
+//
+//  Created by Denis Goloborodko on 6.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRcodeGeneratorAndScanner/QRcodeGeneratorAndScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRcodeGeneratorAndScannerApp.swift
+//  QRcodeGeneratorAndScanner
+//
+//  Created by Denis Goloborodko on 6.07.22.
+//
+
+import SwiftUI
+
+@main
+struct QRcodeGeneratorAndScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
